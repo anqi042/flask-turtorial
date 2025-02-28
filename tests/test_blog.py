6e8a9a5804983b194ad5d2134fa6a5e27c0e8e86
Qi An (NSB) <qi.an@nokia-sbell.com>
2025-02-28 13:50:33 +0800

    add one blog test case

---
+++ tests/test_blog.py
@@ -0,0 +1,8 @@
+from flaskr.blog import blog
+from flaskr.db.db import sqldb as db
+from flaskr.db.db import User, Post, Like, UnLike
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    
